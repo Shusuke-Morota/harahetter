a68d84d29574f4b05b8b8a84a61d1c74eee2853b
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-08-14 21:30:37 +0900

    [Update:]  users / views

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,7 @@
 #     https: false
 #   )
 # end
+
+
+
+Refile.secret_key = 'ecf8b7b9648739ea425599e43890bc5ac36532e8ca203ea7ed718dbb06d43255ecf098c278b3f442a85bcbc3aba8010b1bfd33dcd2c5be5d5297c5f1f923fd33'
