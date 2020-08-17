4ddc5081e3daa8f8302f02cc9b311634216a3cad
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-08-18 00:04:51 +0900

    [Add:]  review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#index"
-  resources :recipes
+  resources :recipes do
+  	resources :reviews, only: [:index, :create]
+  end
+
   resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_16_005955) do
+ActiveRecord::Schema.define(version: 2020_08_17_141756) do
 
   create_table "recipe_images", force: :cascade do |t|
     t.integer "recipe_id"
@@ -25,6 +25,17 @@ ActiveRecord::Schema.define(version: 2020_08_16_005955) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.string "content"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["recipe_id"], name: "index_reviews_on_recipe_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
