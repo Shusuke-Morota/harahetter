4dc7d05275ec3105e8615b7294ed69749b66ce1e
Shusuke-Morota <63095032+Shusuke-Morota@users.noreply.github.com>
2020-08-20 19:31:22 +0900

    [Update:]  recipes / show / comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,25 @@
 class CommentsController < ApplicationController
 	def create
-      @recipe = Recipe.find(params[:recipe_id])
-      #投稿に紐づいたコメントを作成
-      @comment = @recipe.comments.build(comment_params)
-      @comment.user_id = current_user.id
-      @comment.save
-      render :index
-    end
+		@recipe = Recipe.find(params[:recipe_id])
+		@comment = @recipe.comments.build(comment_params)
+		@comment.user_id = current_user.id
+		if @comment.save
+			redirect_to @recipe
+		else
+			render 'recipes/show'
+		end
+	end
 
-    def destroy
-      @comment = Comment.find(params[:id])
-      @comment.destroy
-      render :index
-    end
+	def destroy
+		# @article = Article.find(params[:article_id])
+		@comment = Comment.find(params[:recipe_id])
+		if @comment.destroy
+			redirect_to recipe_path(@comment.recipe)
+		end
+	end
 
-    private
-    def comment_params
-      params.require(:comment).permit(:content, :recipe_id, :user_id)
-    end
+	private
+	def comment_params
+		params.require(:comment).permit(:content, :user_id, :recipe_id)
+	end
 end
